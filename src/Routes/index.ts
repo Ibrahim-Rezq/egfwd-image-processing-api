f8680128249d7ee6c9b13a9f9bdd0f7c515cb7eb
Ibrahim-Rezq <90980686+Ibrahim-Rezq@users.noreply.github.com>
2021-12-23 12:32:23 +0200

    Changes for the udaicty review

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,35 +1,50 @@
-import express from 'express';   
+import express from 'express';
+import { Request, Response } from 'express';
 import * as path from 'path';
 import fs from 'fs';
-import sharp from 'sharp';
+import prossesImg from './prossesingImg';
 
 const imgRoute = express.Router();
 
-imgRoute.get('/', (req, res) => {
-    if(req.query.fileName&&req.query.width&&req.query.height)
-    {
-        const name = req.query.fileName as string;
-        const width = Number(req.query.width) as number ;
-        const height = Number(req.query.height) as number;
-        const imgPath = path.resolve('Assets','images', `${name}.jpg`);
-        const newImgPath = path.resolve('Assets','cache_imgs',`${name}_${width}_${height}'jpg'`);
+imgRoute.get('/', (req: Request, res: Response): void => {
+    const sendData=(data:string):void=>{
+        res.sendFile(data)
+    }
+
+    if (req.query.fileName && req.query.width && req.query.height) {
+        const name: string = req.query.fileName as string;
+        const width: number = Number(req.query.width) as number;
+        const height: number = Number(req.query.height) as number;
+        if (isNaN(width) || isNaN(height)) {
+            res.send('Either width or height is NOT a Vailed Number');
+        } else {
+            fs.existsSync(path.resolve('Assets', 'cache_imgs')) ||
+                fs.mkdir(path.resolve('Assets', 'cache_imgs'), err => {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    console.log('Directory created successfully!');
+                });
+            const imgPath: string = path.resolve(
+                'Assets',
+                'images',
+                `${name}.jpg`
+            );
+            const newImgPath: string = path.resolve(
+                'Assets',
+                'cache_imgs',
+                `${name}_${width}_${height}.jpg`
+            );
             if (fs.existsSync(newImgPath)) {
                 res.sendFile(newImgPath);
+            } else {
+                prossesImg(imgPath,newImgPath,width,height,sendData)
             }
-            else{
-                sharp(imgPath)
-                .rotate()
-                .resize(width,height)
-                .jpeg({ mozjpeg: true })
-                .toBuffer()
-                .then( (data:Buffer) => { fs.writeFileSync(newImgPath, data);})
-                .then(()=> {res.sendFile(newImgPath)})
-                .catch( (err:string) => { res.send("No Such File Was Found") ,console.log(err)});
-            }
-        }else
-        {
-            res.send("The Api Call Needs A Name, Width And Height To Work")
         }
-    });
+    } else {
+        res.send('The Api Call Needs A Name, Width And Height To Work');
+    }
+});
+
 
 export default imgRoute;
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -3,9 +3,13 @@ import app from '../index';
 
 const request = supertest(app);
 describe('Test endpoint responses', () => {
-    it('gets the api endpoint', async (done) => {
+    it('gets the api endpoint', async () => {
         const response = await request.get('/api/images');
         expect(response.status).toBe(200);
-        done();
+    }
+)
+    it('gets the api endpoint to work with Pramters', async () => {
+        const response = await request.get('/api/images?fileName=fjord&width=560&height=900');
+        expect(response.status).toBe(200);
     }
 )});
